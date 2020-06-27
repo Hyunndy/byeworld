eda79d4e8ae00afa7f160250a3cc87c365199086
Hyunndy <yoosa3004@naver.com>
2020-06-28 00:38:11 +0900

    test

--- byeworld/ViewController.swift
+++ byeworld/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 추가 시킨 아웃렛 변수
+    @IBOutlet var IblHello: UILabel!
+    @IBOutlet var IBOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // 추가 시킨 액션 함수
+    // 버튼을 누르면 적용된다.
+    @IBAction func btnSend(_ sender: UIButton) {
+        
+        IblHello.text = "Hello," + IBOutlet.text!
+    }
 }
 
