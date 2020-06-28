34166e12059f7e198d99c1b4ff2bf91499ef4ae0
Hyunndy <yoosa3004@naver.com>
2020-06-28 20:32:40 +0900

    study currentTime

--- byeworld/ViewController.swift
+++ byeworld/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 타이머가 구동되면 실행할 함수를 지정하는 역할
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    // 타이머의 간격 값 -> 1초
+    let interval = 1.0
+    // 타이머가 설정한 간격대로 실행되는지 확인하는 변수
+    var count = 0
+    
+    
     // 현재 시간을 나타내는 라벨
     @IBOutlet var IblCurrentTime: UILabel!
     
@@ -22,6 +30,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // you usually override this method to perform additional initialization on views that were loaded from nib files.
+        
+        // 타이머를 설정하기 위해 사용하는 함수
+        // 1. 타이머 간격, 2. 동작될 view 3. 타이머가 구동될 때 실행할 함수 4. 사용자 정보 5. 반복 여부
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     
     }
     
@@ -34,6 +46,22 @@ class ViewController: UIViewController {
         // 년-월-일 시:분:초 요일
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
         IblPickerTime.text = "선택시간 :" + formatter.string(from: sender.date)
+    }
+    
+    // 타이머가 구동될 때 구동할 함수.
+    // selector 변수에서 정의한 함수 이름과 같아야한다.
+    // scheduledTimer() 에서 인자로 넘겨준 selector 변수와 연결된 함수가 타이머에서 실행된다.
+    // swift4에서는 #selector()의 인자로 사용될 메서드를 선언할 때 오브젝트c와의 호환성을 위해 함수 앞에 반드시 @objc 키워드를 붙여야한다.
+    @objc func updateTime() {
+
+        // 현재 시간을 나타내는 함수
+        let date = NSDate()
+        
+        // 시간을 가져올 수 있는 객체(nsdate(), timepicker 등)를 통해 가져온 시간 정보를 나타낼 포맷을 정하는 객체
+        let formattter = DateFormatter()
+        formattter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        
+        IblCurrentTime.text = "현재시간 :" + formattter.string(from: date as Date)
     }
     
     
