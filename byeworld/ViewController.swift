f6b3a08b63aa31cf612381e25663120d630ca8e7
Hyunndy <yoosa3004@naver.com>
2020-06-28 18:32:07 +0900

    study datepicker

--- byeworld/ViewController.swift
+++ byeworld/ViewController.swift
@@ -9,19 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    // 현재 시간을 나타내는 라벨
+    @IBOutlet var IblCurrentTime: UILabel!
     
-    // 이미지 뷰
-    @IBOutlet var imgView: UIImageView!
+    // 데이트 피커에서 선택한 시간을 나타내는 라벨
+    @IBOutlet var IblPickerTime: UILabel!
     
-    // 현재 이미지 번호
-    var curImgNum: Int = 0
-    
-    // 전체 이미지 번호
-    let maxImgNum: Int = 5
-    
-    // 이미지들
-    var imgOn: UIImage?
-    var imgOff:UIImage?
     
     // 내가 만든 뷰를 불러왔을 때 호출하는 함수.
     override func viewDidLoad() {
@@ -29,40 +23,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // you usually override this method to perform additional initialization on views that were loaded from nib files.
     
-        // 이미지 래핑
-        imgOn = UIImage(named: "커밋1.jpeg")
-        imgOff = UIImage(named: "커밋2.jpeg")
-        
-        imgView.image = imgOn
     }
     
-    // 다음 버튼 클릭
-    @IBAction func nextBtnClicked(_ sender: UIButton) {
+    // 데이트 피커의 액션 함수
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
         
-        if(curImgNum < maxImgNum) {
-            
-            curImgNum += 1
-            
-            if(imgView.image == imgOn) {
-                imgView.image = imgOff
-            }else {
-                imgView.image = imgOn
-            }
-        }
+        // 날짜 포매터
+        let formatter = DateFormatter()
+        
+        // 년-월-일 시:분:초 요일
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        IblPickerTime.text = "선택시간 :" + formatter.string(from: sender.date)
     }
     
-    // 이전 버튼 클릭
-    @IBAction func prevBtnClicked(_ sender: UIButton) {
-        if(curImgNum > 0) {
-             
-             curImgNum -= 1
-             
-             if(imgView.image == imgOn) {
-                 imgView.image = imgOff
-             }else {
-                 imgView.image = imgOn
-             }
-         }
-    }
+    
 }
 
