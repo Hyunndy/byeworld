986e1a3b628b675e1a78e4b2a2fe36a196fad730
Hyunndy <yoosa3004@naver.com>
2020-06-29 15:34:48 +0900

    study alert

--- byeworld/ViewController.swift
+++ byeworld/ViewController.swift
@@ -8,23 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
     
-    // 타이머가 구동되면 실행할 함수를 지정하는 역할
-    let timeSelector: Selector = #selector(ViewController.updateTime)
-    // 타이머의 간격 값 -> 1초
-    let interval = 1.0
-    // 타이머가 설정한 간격대로 실행되는지 확인하는 변수
-    var count = 0
+    // 이미지 뷰 아웃렛 변수
+    @IBOutlet var kermitImg: UIImageView!
     
-    var alarmTime: String = "0"
+    let imgOn = UIImage(named: "커밋1.jpeg")
+    let imgOff = UIImage(named: "커밋2.jpeg")
+    let imgRemove = UIImage(named: "커밋3.jpg")
     
-    
-    // 현재 시간을 나타내는 라벨
-    @IBOutlet var IblCurrentTime: UILabel!
-    
-    // 데이트 피커에서 선택한 시간을 나타내는 라벨
-    @IBOutlet var IblPickerTime: UILabel!
+    var isRampOn: Bool = true
     
     
     // 내가 만든 뷰를 불러왔을 때 호출하는 함수.
@@ -33,48 +26,82 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // you usually override this method to perform additional initialization on views that were loaded from nib files.
         
-        // 타이머를 설정하기 위해 사용하는 함수
-        // 1. 타이머 간격, 2. 동작될 view 3. 타이머가 구동될 때 실행할 함수 4. 사용자 정보 5. 반복 여부
-        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
-    
+        kermitImg.image = imgOn
     }
     
-    // 데이트 피커의 액션 함수
-    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+    // 켜기 버튼 눌렀을 때 일어나는 액션 함수
+    @IBAction func btnImgOn(_ sender: UIButton) {
         
-        // 날짜 포매터
-        let formatter = DateFormatter()
+        // 이미 전구가 켜져있는 경우
+        if(isRampOn) {
+            
+            /* 전구가 켜져있다는 alert 보냄 */
+            // 1. UIAlertController 생성
+            let imgOnAlert = UIAlertController(title: "경고!", message: "현재 On 상태 입니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            // 2. UIAlertAction을 생성
+            let onAction = UIAlertAction(title: "네, 알겠습니다.", style: UIAlertAction.Style.default, handler: nil)
+            
+            // 3. 생성된 onAction 을 alert에 추가한다.
+            imgOnAlert.addAction(onAction)
+            
+            // 4. present 메서드 실행 실행
+            present(imgOnAlert, animated: true, completion: nil)
+            
+        }
+        // 전구가 꺼져 있는 경우
+        else {
+            kermitImg.image = imgOn
+            isRampOn = true
+        }
         
-        // 년-월-일 시:분:초 요일
-        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
-        IblPickerTime.text = "선택시간 :" + formatter.string(from: sender.date)
-        
-        // 알람 타임
-        alarmTime = formatter.string(from: sender.date)
     }
     
-    // 타이머가 구동될 때 구동할 함수.
-    // selector 변수에서 정의한 함수 이름과 같아야한다.
-    // scheduledTimer() 에서 인자로 넘겨준 selector 변수와 연결된 함수가 타이머에서 실행된다.
-    // swift4에서는 #selector()의 인자로 사용될 메서드를 선언할 때 오브젝트c와의 호환성을 위해 함수 앞에 반드시 @objc 키워드를 붙여야한다.
-    @objc func updateTime() {
-
-        // 현재 시간을 나타내는 함수
-        let date = NSDate()
+    
+    @IBAction func btnImgOff(_ sender: UIButton) {
         
-        // 시간을 가져올 수 있는 객체(nsdate(), timepicker 등)를 통해 가져온 시간 정보를 나타낼 포맷을 정하는 객체
-        let formattter = DateFormatter()
-        formattter.dateFormat = "yyyy-MM-dd HH:mm EEE"
-        
-        IblCurrentTime.text = "현재시간 :" + formattter.string(from: date as Date)
-        
-        if(formattter.string(from: date as Date) == alarmTime) {
-            view.backgroundColor = UIColor.red
-        } else {
-            view.backgroundColor = UIColor.white
+        if(isRampOn) {
+            
+            let imgOffAlert = UIAlertController(title: "램프 끄기", message: "커밋2로 전환할까요?", preferredStyle: UIAlertController.Style.alert)
+            
+            // 액션에 해당하는 작업을 handler 에 넣어준다.
+            // self 를 넣어줘야 에러가 발생하지 않으며, 익명 함수(클로저) 형태이다.
+            let offAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default,
+                                          handler: { ACTION -> () in
+                                            self.kermitImg.image = self.imgOff
+                                            self.isRampOn = false })
+            
+            let cancelAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
+            
+            imgOffAlert.addAction(offAction)
+            imgOffAlert.addAction(cancelAction)
+            
+            present(imgOffAlert, animated: true, completion: nil)
         }
     }
     
+    @IBAction func btnImgRemove(_ sender: UIButton) {
+        
+        let imgRemoveAlert = UIAlertController(title: "램프 제거", message: "커밋3로 전환할까요?", preferredStyle: UIAlertController.Style.alert)
+        
+        let offAction = UIAlertAction(title: "아뇨 전 커밋2로 전환할래요", style: UIAlertAction.Style.default, handler: { ACTION in self.kermitImg.image = self.imgOff
+            self.isRampOn = false
+        })
+        
+        let onAction = UIAlertAction(title: "아뇨 전 커밋1로 전환할래요", style: UIAlertAction.Style.default, handler: { ACTION in self.kermitImg.image = self.imgOn
+            self.isRampOn = true
+        })
+        
+        let removeAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default, handler: { ACTION in self.kermitImg.image = self.imgRemove
+            self.isRampOn = false
+        })
+        
+        imgRemoveAlert.addAction(offAction)
+        imgRemoveAlert.addAction(onAction)
+        imgRemoveAlert.addAction(removeAction)
+        
+        present(imgRemoveAlert, animated: true, completion: nil)
+    }
     
 }
 
