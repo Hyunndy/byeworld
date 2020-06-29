60a13a4d63bbdcf12c43723c0ee373cd328fdcfc
Hyunndy <yoosa3004@naver.com>
2020-06-29 21:52:37 +0900

    study horizontal Swipe + ContainerView + other Storyboard

---
+++ byeworld/LeftViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LeftViewController.swift
+//  byeworld
+//
+//  Created by user on 2020/06/29.
+//  Copyright © 2020 Hyunndy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class LeftViewController: UIViewController {
+
+    // 내가 만든 뷰를 불러왔을 때 호출하는 함수.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // you usually override this method to perform additional initialization on views that were loaded from nib files.
+    }
+    
+}
